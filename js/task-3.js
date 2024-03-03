dc68af30d91459a26e65eccbe72c5b5933aa4f15
Toni777D <39744158+Toni777D@users.noreply.github.com>
2024-03-03 11:40:00 +0100

    goit-js-hw-2.1

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,25 @@
-function getElementWidth(content, padding, border) {
-  const box = Number.parseInt(content);
-  const pad = Number.parseInt(padding);
-  const bor = Number.parseFloat(border);
-  return box + pad * 2 + bor * 2;
+function checkForSpam(message) {
+  const checkSpamSale = message.toLowerCase();
+  const hasWordSpam = checkSpamSale.includes("spam");
+  const hasWordSale = checkSpamSale.includes("sale");
+  if (hasWordSpam || hasWordSale) {
+    return Boolean;
+  } else {
+    return Boolean;
+  }
 }
+checkForSpam("Latest technology news");
+checkForSpam("JavaScript weekly newsletter");
+checkForSpam("Get best sale offers now!");
+checkForSpam("Amazing SalE, only tonight!");
+checkForSpam("Trust me, this is not a spam message");
+checkForSpam("Get rid of sPaM emails. Our book in on sale!");
+checkForSpam("[SPAM] How to earn fast money?");
 
-getElementWidth();
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
